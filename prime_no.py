9558301375df0a2bd689b35b2a4e8e4c645f84e9
Soumyajit Mishra <soumyajit.mishra@CALSOFTHQ.COM>
2017-04-24 18:33:38 +0530

    Added some documentations

--- prime_no.py
+++ prime_no.py
@@ -1,5 +1,5 @@
 # This program is to find out prime numbers between 0 to 100
-def is_prime(n):
+def isPrime(n):
     if n == 2:
         return True
     if n % 2 == 0 or n <= 1:
@@ -9,15 +9,16 @@ def is_prime(n):
             return False
     return True
 
+# Declaring two blank lists to store prime and non-prime numbers
 primeList = []
 nonPrimeList = []
+
 for i in range(100):
-    primeNo = is_prime(i)
+    primeNo = isPrime(i)
     if primeNo == True:
         primeList.append(i)
     else:
         nonPrimeList.append(i)
-        
 
 print "Prime numbers between 0 to 100 are: ", primeList
 print "Non Prime numbers between 0 to 100 are: ", nonPrimeList
