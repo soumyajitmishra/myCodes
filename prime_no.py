c2731fefb14a6e506ae85aa01e577598bbb5f6a8
soumyajitmishra <soumyajitmishra368@gmail.com>
2017-04-24 18:02:55 +0530

    Update prime_no.py

--- prime_no.py
+++ prime_no.py
@@ -1,18 +1,17 @@
+# This program is to find out prime numbers between 0 to 100
 def is_prime(n):
     if n == 2:
         return True
     if n % 2 == 0 or n <= 1:
         return False
-
     for divisor in range(3, n, 1):
         if n % divisor == 0:
             return False
     return True
 
 for i in range(100):
-    
-    a = is_prime(i)
-    if a == True:
+    primeNo = is_prime(i)
+    if primeNo == True:
         print "%d is a Prime no" % i
     else:
         print "%d is not a Prime no" % i
